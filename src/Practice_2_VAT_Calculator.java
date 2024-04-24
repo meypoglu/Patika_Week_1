d618c141ae4ce83c65578170abc6944b64391843
meyup <m.eypoglu@gmail.com>
2024-04-24 12:03:57 +0300

    Patika+ Java bootcamp week 1 technical content part 1

---
+++ src/Practice_2_VAT_Calculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Practice_2_VAT_Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Para değerini giriniz: ");
+        double money = input.nextDouble();
+        double vat = money > 999 ? 0.08d : 0.18d;
+        System.out.println("KDV'li tutar: " + (money + money * vat));
+        System.out.println("KDV tutarı: " + vat);
+    }
+}
---
+++ src/Practice_4_Taximeter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Practice_4_Taximeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Gidilen mesafeyi giriniz: ");
+        double dist = input.nextDouble();
+
+        double priceKm = 2.20d;
+        double total = (dist * priceKm) + 10 < 20.0d ? 20.0d : (dist * priceKm) + 10;
+        System.out.println("Toplam Tutar: " + total);
+    }
+}
